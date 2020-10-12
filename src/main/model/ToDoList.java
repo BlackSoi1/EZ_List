8b47419e2813d584a82109ab168755ca7312071f
Xiaolong Li <xia01ong@students.ubc.ca>
2020-10-11 18:11:09 -0700

    fix more code coverage problem

--- src/main/model/ToDoList.java
+++ src/main/model/ToDoList.java
@@ -18,21 +18,24 @@ public class ToDoList {
     }
 
     //MODIFIES: this,task
-    //EFFECTS: add task to the to-do list
-    public void addTask(Tasks task) {
-        toDoList.add(task);
-    }
-
-    //MODIFIES: this
-    //EFFECTS: If the to-do list size is 0 return false, else delete the task from the to-do list and return true.
-    public boolean deleteTask(Tasks task) {
-        if (toDoList.size() == 0) {
-            return false;
-        } else {
-            toDoList.remove(task);
+    //EFFECTS: if the task is not on the to-do list add task to the to-do list
+    //and return true else return false
+    public boolean addTask(Tasks task) {
+        if (!toDoList.contains(task)) {
+            toDoList.add(task);
             return true;
+        } else {
+            return false;
         }
 
+    }
+
+
+    //REQUIRES:  the to-do list size is not 0
+    //MODIFIES: this
+    //EFFECTS: delete the task from the to-do list.
+    public void deleteTask(Tasks task) {
+        toDoList.remove(task);
     }
 
     //EFFECTS: return the size of the to do list
--- src/test/model/ToDoListTest.java
+++ src/test/model/ToDoListTest.java
@@ -17,46 +17,50 @@ public class ToDoListTest {
         task.setStatus(false);
         task.setPriority(1);
         toDoList = new ToDoList("list1");
-        toDoList.addTask(task);
     }
 
     @Test
     void testAddTask() {
+        toDoList.addTask(task);
         assertEquals(toDoList.getTask(0), task);
         assertEquals(toDoList.size(), 1);
-        assertEquals(toDoList.getTask(0).getName(),"Task2");
-        assertEquals(toDoList.getTask(0).getInfo(),"Test2");
-    }
-
-    @Test
-    void testDeleteTask() {
-        assertEquals(toDoList.getTask(0), task);
-        assertEquals(toDoList.size(), 1);
-        toDoList.deleteTask(task);
-        assertEquals(toDoList.size(), 0);
-        boolean flag = toDoList.deleteTask(task);
+        assertEquals(toDoList.getTask(0).getName(), "Task2");
+        assertEquals(toDoList.getTask(0).getInfo(), "Test2");
+        boolean flag = toDoList.addTask(task);
         assertEquals(flag,false);
     }
 
     @Test
+    void testDeleteTask() {
+        toDoList.addTask(task);
+        assertEquals(toDoList.getTask(0), task);
+        assertEquals(toDoList.size(), 1);
+        toDoList.deleteTask(task);
+        assertEquals(toDoList.size(), 0);
+    }
+
+    @Test
     void testSize() {
+        toDoList.addTask(task);
         assertEquals(toDoList.size(), 1);
         Tasks other2 = new Tasks("Task3", "Test3");
-        toDoList.addTask(task);
+        toDoList.addTask(other2);
         assertEquals(toDoList.size(), 2);
     }
 
     @Test
     void testGetTask() {
+        toDoList.addTask(task);
         Tasks other = toDoList.getTask(0);
-        assertEquals(other.getName(),"Task2");
-        assertEquals(other.getInfo(),"Test2");
-        assertEquals(other.getStatus(),false);
-        assertEquals(other.getPriority(),1);
+        assertEquals(other.getName(), "Task2");
+        assertEquals(other.getInfo(), "Test2");
+        assertEquals(other.getStatus(), false);
+        assertEquals(other.getPriority(), 1);
     }
 
     @Test
     void testGetListName() {
-        assertEquals(toDoList.getListName(),"list1");
+        toDoList.addTask(task);
+        assertEquals(toDoList.getListName(), "list1");
     }
 }
