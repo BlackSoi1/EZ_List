05f6279032e8f08c35066d67e6974e28ea54426b
Xiaolong Li <xia01ong@students.cs.ubc.ca>
2020-10-22 17:29:00 -0700

    Fix the code coverage problem

--- src/main/exception/ListSizeZeroException.java
+++ src/main/exception/ListSizeZeroException.java
@@ -1,10 +1,6 @@
 package exception;
 
 public class ListSizeZeroException extends Exception {
-    public ListSizeZeroException() {
-        super();
-    }
-
     public ListSizeZeroException(String message) {
         super(message);
     }
--- src/main/exception/OutOfRangeException.java
+++ src/main/exception/OutOfRangeException.java
@@ -1,10 +1,6 @@
 package exception;
 
 public class OutOfRangeException extends Exception {
-    public OutOfRangeException() {
-        super();
-    }
-
     public OutOfRangeException(String message) {
         super(message);
     }
